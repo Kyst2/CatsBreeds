13e3dd06e7c8fa6c0171305bd3acd8ab275141c9
AndrewKust <deradus@ukr.net>
2024-11-27 17:21:38 +0200

    TestApp.swift -> CatsAndDogs.swift

--- CatInfo.swift
+++ CatInfo.swift
@@ -1,23 +1,43 @@
 import Foundation
 
-struct CatInfo: Codable {
-    let id: String
-    let width: Int
-    let height: Int
-    let url: String
-    let breeds: [Breed]
-}
-
-struct Breed: Codable {
+struct CatBreed: Codable {
     let weight: Weight
     let id: String
     let name: String
+    let cfaURL: String?
+    let vetstreetURL: String?
+    let vcahospitalsURL: String?
     let temperament: String
     let origin: String
     let countryCodes: String
     let countryCode: String
+    let description: String
     let lifeSpan: String
-    let wikipediaURL: String?
+    let indoor: Int
+    let lap: Int?
+    let altNames: String
+    let adaptability: Int
+    let affectionLevel: Int
+    let childFriendly: Int
+    let dogFriendly: Int
+    let energyLevel: Int
+    let grooming: Int
+    let healthIssues: Int
+    let intelligence: Int
+    let sheddingLevel: Int
+    let socialNeeds: Int
+    let strangerFriendly: Int
+    let vocalisation: Int
+    let experimental: Int
+    let hairless: Int
+    let natural: Int
+    let rare: Int
+    let rex: Int
+    let suppressedTail: Int
+    let shortLegs: Int
+    let wikipediaURL: String
+    let hypoallergenic: Int
+    let referenceImageID: String
 }
 
 struct Weight: Codable {
--- CatsAndDogs.swift
+++ CatsAndDogs.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 @main
-struct TestAppApp: App {
+struct CatsAndDogs: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
